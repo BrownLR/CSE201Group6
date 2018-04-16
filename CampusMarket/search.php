a32988c54d3ea5cfe8d0f956a6e619504aa6ce14
Logan Brown <brownlr4@miamioh.edu>
2018-04-16 15:35:24 -0400

    minor bugs

--- CampusMarket/search.php
+++ CampusMarket/search.php
@@ -94,7 +94,7 @@ $item = 0;
 		<nav class="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left" style="display:none;z-index:2;width:40%;min-width:300px" id="mySidebar">
 		  <a href="javascript:void(0)" onclick="w3_close()"
 		  class="w3-bar-item w3-button">Close Menu</a>
-		  <a href="http://localhost/Home.php#items" onclick="w3_close()" class="w3-bar-item w3-button">Items</a>
+		  <a href="http://localhost/Home.php#items" onclick="w3_close()" class="w3-bar-item w3-button">Home</a>
 		  <a href="http://localhost/Home.php#add" onclick="w3_close()" class="w3-bar-item w3-button">Add Listing</a>
 		  <a href="http://localhost/Home.php#about" onclick="w3_close()" class="w3-bar-item w3-button">About</a>
 		</nav>
