5a17e417853d611c991ca954564e7b863b4e31d3
Logan Brown <brownlr4@miamioh.edu>
2018-04-16 15:54:52 -0400

    minor bugs again

--- CampusMarket/Home.php
+++ CampusMarket/Home.php
@@ -56,7 +56,7 @@
 		<nav class="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left" style="display:none;z-index:2;width:40%;min-width:300px" id="mySidebar">
 		  <a href="javascript:void(0)" onclick="w3_close()"
 		  class="w3-bar-item w3-button">Close Menu</a>
-		  <a href="#items" onclick="w3_close()" class="w3-bar-item w3-button">Home</a>
+		  <a href="http://localhost/Home.php" onclick="w3_close()" class="w3-bar-item w3-button">Home</a>
 		  <a href="#add" onclick="w3_close()" class="w3-bar-item w3-button">Add Listing</a>
 		  <a href="#about" onclick="w3_close()" class="w3-bar-item w3-button">About</a>
 		</nav>
